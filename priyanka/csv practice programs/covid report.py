fae990430d326fcac69e2bea1f8f0acc413796fa
Bpriyanka0 <132559526+Bpriyanka0@users.noreply.github.com>
2023-05-08 13:13:26 +0530

    Add files via upload

---
+++ priyanka/csv practice programs/covid report.py
@@ -0,0 +1,9 @@
+import csv
+
+with open("C:/Users/DELL/OneDrive/Desktop/report.csv","r")as file_object:
+
+    data_table =csv.reader(file_object)
+
+    for line in data_table:
+
+        print(line[1])
---
+++ priyanka/csv practice programs/ipl.py
@@ -0,0 +1,11 @@
+import csv
+
+with open("C:/Users/DELL/OneDrive/Desktop/ipl.csv","r") as file_object:
+
+    data_table = csv.reader(file_object)
+
+    for line in data_table:
+
+        print(line[1])
+
+    
